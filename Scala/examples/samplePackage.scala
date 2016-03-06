9a34ff0e70b8b73d2f3a92680356959e61beb76c
muhammadmohsin1994 <muhammad.mohsin1994@gmail.com>
2016-03-06 07:16:16 +0500

    Removed libs

--- Scala/examples/samplePackage.scala
+++
@@ -1,9 +0,0 @@
-// Example of creating a package
-// Compile package using scalac and then you can import it
-package samplePackage
-
-class sampleClass {
-  def sampleMethod = {
-    new util.Random().nextInt
-  }
-}
